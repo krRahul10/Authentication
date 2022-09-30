02b8d05da7f43bcc9b2d51561ee0128e1069083f
Rahul <rahul10kr7@gmail.com>
2022-09-30 18:02:01 +0530

    authentication done

--- Authmern/frontend/src/App.js
+++ Authmern/frontend/src/App.js
@@ -1,20 +1,73 @@
 import { Header } from "./Components/Header";
 import { Login } from "./Components/Login";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { Register } from "./Components/Register";
 import { Dashboard } from "./Components/Dashboard";
 import { Error } from "./Components/Error";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import { useContext, useEffect, useState } from "react";
+import { LoginContext } from "./ContextProvider/Context";
 
 function App() {
+  const [data, setData] = useState(false);
+  const { logindata, setLoginData } = useContext(LoginContext);
+  const history = useNavigate();
+
+  const DashBoardValid = async () => {
+    const token = JSON.parse(localStorage.getItem("usersdatatoken"));
+
+    const res = await fetch("http://localhost:8080/validuser", {
+      method: "GET",
+      headers: {
+        Authorization: token,
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+
+    if (!data || data.status === 401) {
+      console.log("user not valid");
+      // history("*");
+    } else {
+      //   console.log("user verify");
+      setLoginData(data);
+      history("/dashboard");
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      DashBoardValid();
+      setData(true);
+    }, 3000);
+  }, []);
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/*" element={<Error />} />
-      </Routes>
+      {data ? (
+        <>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/*" element={<Error />} />
+          </Routes>
+        </>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          Loading.... &nbsp;
+          <CircularProgress />
+        </Box>
+      )}
     </>
   );
 }
--- Authmern/frontend/src/Components/Dashboard.jsx
+++ Authmern/frontend/src/Components/Dashboard.jsx
@@ -1,9 +1,12 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { LoginContext } from "../ContextProvider/Context";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 export const Dashboard = () => {
   const { logindata, setLoginData } = useContext(LoginContext);
+  const [data, setData] = useState(false);
   const history = useNavigate();
 
   const DashBoardValid = async () => {
@@ -30,25 +33,46 @@ export const Dashboard = () => {
   };
 
   useEffect(() => {
-    DashBoardValid();
+    setTimeout(() => {
+      DashBoardValid();
+      setData(true);
+    }, 3000);
   }, []);
   return (
     <div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItem: "center",
-          margin: "30px 0px 0px 35%",
-        }}
-      >
-        <img
-          src="./man.png"
-          alt=""
-          style={{ width: "300px", height: 200, marginTop: "20px" }}
-        />
-        <h1>User Email: {logindata ? logindata.validUserOne.email : ""}</h1>
-      </div>
+      {data ? (
+        <>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItem: "center",
+              margin: "30px 0px 0px 35%",
+            }}
+          >
+            <img
+              src="./man.png"
+              alt=""
+              style={{ width: "300px", height: 200, marginTop: "20px" }}
+            />
+            <h1>User Email: {logindata ? logindata.validUserOne.email : ""}</h1>
+          </div>
+        </>
+      ) : (
+        <>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
+            }}
+          >
+            Loading.... &nbsp;
+            <CircularProgress />
+          </Box>
+        </>
+      )}
     </div>
   );
 };
